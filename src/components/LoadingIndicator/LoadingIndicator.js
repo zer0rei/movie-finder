9694c40b9f99b8bb00d72eeb258d74a56a3e4b84
Elgaoussi Hamza <rei-zer0@live.com>
2019-11-27 19:20:11 +0100

    Add LoadingIndicator component

---
+++ src/components/LoadingIndicator/LoadingIndicator.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./LoadingIndicator.css";
+
+function LoadingIndicator({ className }) {
+  return (
+    <div className={`spinner ${className}`}>
+      <div className="double-bounce1" />
+      <div className="double-bounce2" />
+    </div>
+  );
+}
+
+LoadingIndicator.propTypes = {
+  className: PropTypes.string
+};
+
+export default LoadingIndicator;
