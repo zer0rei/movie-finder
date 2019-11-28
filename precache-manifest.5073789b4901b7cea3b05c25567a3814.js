496f0a6fb95f69e4752449d7380642896d320148
Elgaoussi Hamza <rei-zer0@live.com>
2019-11-29 00:01:34 +0100

    Updates

---
+++ precache-manifest.5073789b4901b7cea3b05c25567a3814.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8c2ff99755dd8312e7f18e8372a46e",
+    "url": "/movie-finder/index.html"
+  },
+  {
+    "revision": "4582d6966621b0e532c6",
+    "url": "/movie-finder/static/css/2.942c9c19.chunk.css"
+  },
+  {
+    "revision": "08b66f21d9c0044556b5",
+    "url": "/movie-finder/static/css/main.c703c14a.chunk.css"
+  },
+  {
+    "revision": "4582d6966621b0e532c6",
+    "url": "/movie-finder/static/js/2.d7be50c4.chunk.js"
+  },
+  {
+    "revision": "08b66f21d9c0044556b5",
+    "url": "/movie-finder/static/js/main.901b327e.chunk.js"
+  },
+  {
+    "revision": "ef4324cc2ae6142022f2",
+    "url": "/movie-finder/static/js/runtime-main.15c8f900.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/movie-finder/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/movie-finder/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/movie-finder/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/movie-finder/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/movie-finder/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/movie-finder/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/movie-finder/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/movie-finder/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/movie-finder/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/movie-finder/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/movie-finder/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/movie-finder/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/movie-finder/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/movie-finder/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/movie-finder/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "bb4244da55a0919076ce52696841083c",
+    "url": "/movie-finder/static/media/logo.bb4244da.svg"
+  },
+  {
+    "revision": "9231dfe543f92cc27daec7bee8d26d97",
+    "url": "/movie-finder/static/media/movie-clapper.9231dfe5.svg"
+  },
+  {
+    "revision": "41723b11516ea78257786cc7f17c6144",
+    "url": "/movie-finder/static/media/television.41723b11.svg"
+  }
+]);
